95d38a5fc3010aa91ed79a3bb4cd52a573fceb65
abhishandilya <ashandilya@greenactionstudio.com>
2023-05-20 06:07:11 -0400

    test: configure coverage and codecov

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-network-helpers"
+import "@nomicfoundation/hardhat-network-helpers";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
